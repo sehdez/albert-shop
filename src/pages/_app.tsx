ee5fe637cebdcd228425c74aa28eeb5fd99468a1
Programatore1 <Programatore1@sitiorandom.com>
2023-02-16 20:30:30 -0600

    primeros ajustes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import type { AppProps } from 'next/app'
+import { lightTheme } from "../themes";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={ lightTheme }>
+            <CssBaseline/>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
